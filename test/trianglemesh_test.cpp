2f3fc67355fc221a56bf88b082591f0dd35a30ed
Amorilia <amorilia@users.sourceforge.net>
2009-10-18 16:57:09 +0100

    Face winding test.

--- test/trianglemesh_test.cpp
+++ test/trianglemesh_test.cpp
@@ -57,6 +57,21 @@ BOOST_AUTO_TEST_CASE(edge_common_test_2) {
 	BOOST_CHECK_EQUAL(common[1], 13);
 }
 
+BOOST_AUTO_TEST_CASE(face_winding_test) {
+	Face f(5, 6, 7);
+	// throw on illegal index?
+	BOOST_CHECK_THROW(f.get_vertex_winding(5, 8), std::runtime_error);
+	BOOST_CHECK_THROW(f.get_vertex_winding(8, 5), std::runtime_error);
+	BOOST_CHECK_THROW(f.get_vertex_winding(8, 9), std::runtime_error);
+	// correct winding?
+	BOOST_CHECK_EQUAL(f.get_vertex_winding(5, 6), 0);
+	BOOST_CHECK_EQUAL(f.get_vertex_winding(6, 7), 0);
+	BOOST_CHECK_EQUAL(f.get_vertex_winding(7, 5), 0);
+	BOOST_CHECK_EQUAL(f.get_vertex_winding(6, 5), 1);
+	BOOST_CHECK_EQUAL(f.get_vertex_winding(7, 6), 1);
+	BOOST_CHECK_EQUAL(f.get_vertex_winding(5, 7), 1);
+}
+
 BOOST_AUTO_TEST_CASE(add_face_test) {
 	Mesh m;
 
