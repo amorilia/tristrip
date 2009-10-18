acd9cb803e15eb3a4e1ff47f8dee951251b9a030
Amorilia <amorilia@users.sourceforge.net>
2009-10-18 16:21:09 +0100

    Added cmake build script and initial regression test.

---
+++ test/trianglemesh_test.cpp
@@ -0,0 +1,24 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include "trianglemesh.hpp"
+
+BOOST_AUTO_TEST_SUITE(trianglemesh_test_suite)
+
+BOOST_AUTO_TEST_CASE(declare_test) {
+  BOOST_CHECK_NO_THROW(Edge edge(0, 1));
+  BOOST_CHECK_NO_THROW(Face face(0, 1, 2));
+	BOOST_CHECK_NO_THROW(Mesh mesh);
+}
+
+BOOST_AUTO_TEST_CASE(add_face_test) {
+	Mesh m;
+
+	// add faces
+	BOOST_CHECK_NO_THROW(m.add_face(0, 1, 2));
+	BOOST_CHECK_NO_THROW(m.add_face(2, 1, 3));
+	BOOST_CHECK_NO_THROW(m.add_face(2, 3, 4));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
